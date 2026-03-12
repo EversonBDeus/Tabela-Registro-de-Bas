7ed6f4c25f2928288a21ea87b9020dc6e9062e3d
EversonBDeus <everson.deus@outlook.com.br>
2026-03-12 17:29:11 -0400

    fix: remover configuracao tipada do color mode do nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,9 @@
-﻿export default defineNuxtConfig({
+﻿import { defineNuxtConfig } from 'nuxt/config'
+
+export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
 
   modules: ['@nuxt/ui', '@pinia/nuxt', '@nuxtjs/color-mode'],
   css: ['./app/assets/css/main.css'],
-
-  colorMode: {
-    preference: 'system',
-    fallback: 'light',
-    classSuffix: ''
-  }
 })
